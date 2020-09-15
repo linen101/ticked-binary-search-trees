1287c3eabfc9f4d97f50c2a40be478d1a08b9455
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2020-09-16 00:04:31 +0300

    Add minor and mostly cosmetic changes

--- BSTree1.hs
+++ BSTree1.hs
@@ -1,14 +1,14 @@
-module  BSTree (BSTree, size, height, insert, search, root) where
+module BSTree (BSTree, size, height, insert, search, root) where
 
 {-@ type Nat = {v:Int | 0 <= v} @-}
 
 {-@ type NEBSTree a = {t: BSTree a | 0 < size t} @-}
 
-{-@ data BSTree a = Nil 
+{-@ data BSTree a = Nil
                   | Node { x :: a
-                         , l :: BSTree {v:a | v < x }  
+                         , l :: BSTree {v:a | v < x }
                          , r :: BSTree {v:a | v > x } }
-                         @-}
+  @-}
 data BSTree a = Nil | Node a (BSTree a) (BSTree a) deriving Show
 
 {-@ measure size @-}
@@ -18,30 +18,30 @@ size Nil = 0
 size (Node x l r) = 1 + size l + size r
 {-@ invariant {v:BSTree a | 0 <= size v} @-}
 
-{-@  measure height @-}
+{-@ measure height @-}
 {-@ height :: BSTree a -> Nat @-}
 height :: BSTree a -> Int
 height Nil = 0
-height (Node x l r) = if (height l) > (height r) then (1 + height l) else (1 + height r)
+height (Node x l r) = 1 + max (height l) (height r)
 {-@ invariant {v:BSTree a | 0 <= height v} @-}
 
-{-@ insert :: Ord a => t:BSTree a -> x:a -> {t':BSTree a | size t' = size t || size t' = size t + 1} @-}
+{-@ insert :: Ord a => t:BSTree a -> x:a ->
+              {t':BSTree a | size t' = size t || size t' = size t + 1} @-}
 insert :: Ord a => BSTree a -> a -> BSTree a
 insert Nil x = Node x Nil Nil
 insert t@(Node y l r) x
-    | x < y = (Node y (insert l x) r) 
-    | x > y = (Node y l (insert r x))
-    | x == y = t
+    | x < y      =  Node y (insert l x) r
+    | x > y      =  Node y l (insert r x)
+    | otherwise  =  t
 
 {-@ search :: Ord a => BSTree a -> a -> Bool @-}
 search :: Ord a => BSTree a -> a -> Bool
 search Nil _ = False
 search (Node y l r) x
-    | x == y = True
-    | x < y = search l x
-    | x > y = search r x 
+    | x == y     =  True
+    | x < y      =  search l x
+    | otherwise  = search r x
 
-
-{-@ root :: NEBSTree a -> a @-} 
-root :: BSTree a -> a 
+{-@ root :: NEBSTree a -> a @-}
+root :: BSTree a -> a
 root (Node x _ _) = x
--- Tree2.hs
+++ Tree2.hs
@@ -6,15 +6,15 @@ type Nat = Int
 {-@ type NETree a = {t: Tree a | 0 < size t} @-}
 type NETree a = Tree a
 
-{-@ data Tree [size]  @-} 
+{-@ data Tree [size] @-}
 -- or: data Tree a = Nil | Node (x::a) (l::Tree a) (r::Tree a)
 data Tree a = Nil | Node a (Tree a) (Tree a) deriving Show
 
-{-@  measure height @-}
+{-@ measure height @-}
 {-@ height :: Tree a -> Nat @-}
 height :: Tree a -> Int
 height Nil = 0
-height (Node x l r) = if (height l) > (height r) then (1 + height l) else (1 + height r)
+height (Node x l r) = 1 + max (height l) (height r)
 {-@ invariant {v:Tree a | 0 <= height v} @-}
 
 {-@ measure size @-}
@@ -24,14 +24,14 @@ size Nil = 0
 size (Node x l r) = 1 + size l + size r
 {-@ invariant {v:Tree a | 0 <= size v} @-}
 
-{-@ silly_insert :: Eq a=> t:Tree a -> x:a -> {t':Tree a | size t' == 1 + size t || size t' = size t}  @-}
+{-@ silly_insert :: Eq a => t:Tree a -> x:a -> {t':Tree a | size t' == 1 + size t || size t' = size t}  @-}
 silly_insert :: Eq a => Tree a -> a -> Tree a
 silly_insert Nil x = Node x Nil Nil
-silly_insert t@(Node y l r) x 
-    | (height l) > (height r) && x /=y = (Node y l (Node x r Nil)) 
-    | (height l) <= (height r) && x /=y = (Node y (Node x l Nil) r)
-    | otherwise = t
+silly_insert t@(Node y l r) x
+    | x == y               =  t
+    | height l > height r  =  Node y l (Node x r Nil)
+    | otherwise            =  Node y (Node x l Nil) r
 
-{-@ root :: NETree a -> a @-} 
-root :: Tree a -> a 
+{-@ root :: NETree a -> a @-}
+root :: Tree a -> a
 root (Node x _ _) = x
