1419d7c8ccf94809e9e21500f78469e56d05fda0
linen <linen@protonmail.com>
2020-12-21 21:55:46 +0200

    add order invariant

--- src/RBTree.hs
+++ src/RBTree.hs
@@ -2,7 +2,7 @@
 {-@ LIQUID "--ple-local"  @-}
 
 
-module RBTree (RBTree(..), Maybe(..), height, size) where
+module RBTree (Tree(..), Maybe(..), height, size) where
 
 import Functions_Types (max, min, Nat, Maybe(..))
 import Prelude hiding (Applicative(..), Monad(..), Maybe(..), max, min, fmap)
@@ -10,28 +10,10 @@ import Prelude hiding (Applicative(..), Monad(..), Maybe(..), max, min, fmap)
 import Language.Haskell.Liquid.RTick
 import Language.Haskell.Liquid.RTick.Combinators
 
--------------------------------------------------------------------------------
--- | Datatypes:
--------------------------------------------------------------------------------
 
-{-@ data RBTree k v <l :: root:k -> x:k -> Bool, r :: root:k -> x:k -> Bool>
-        = Nil
-        | Node { col :: Color
-               , key :: k
-               , val :: v
-               , tl  :: RBTree <l, r> (k <l key>) v
-               , tr  :: RBTree <l, r> (k <r key>) v }
-@-}
-
-data RBTree k v = Nil | Node Color k v (RBTree k v) (RBTree k v) deriving Show
+data Tree k v = Nil | Node Color k v !(Tree k v) !(Tree k v) deriving Show
 
 data Color = R | B deriving (Eq,Show)
-
-{-@ type ORBT k v = RBTree <{\root k -> k < root}, {\root k -> k > root}> k v @-}
-
-{-@ type NERB k v = {t: RBTree k v | 0 < size t} @-}
-
-{-@ type NERBT k v = {t: ORBT k v | 0 < size t } @-}
 
 -------------------------------------------------------------------------------
 -- | Measures:
@@ -40,55 +22,65 @@ data Color = R | B deriving (Eq,Show)
 --  size invariant  --  
 
 {-@ measure size @-}
-{-@ size :: RBTree k v -> Nat @-}
-size :: RBTree k v -> Int
+{-@ size :: Tree k v -> Nat @-}
+size :: Tree k v -> Int
 size Nil             = 0
 size (Node _ _ _ l r) = 1 + size l + size r
-{-@ invariant {t:RBTree k v | 0 <= size t} @-}
+{-@ invariant {t:Tree k v | 0 <= size t} @-}
 
 --  height invariant   --
 
 {-@ measure height @-}
-{-@ height :: RBTree k v -> Nat @-}
-height :: RBTree k v -> Int
+{-@ height :: Tree k v -> Nat @-}
+height :: Tree k v -> Int
 height Nil              = 0
 height (Node _ _ _ l r) = 1 + max (height l) (height r)
-{-@ invariant {t:RBTree k v | 0 <= height t} @-}
-
---	black rooted Tree 	--
-
-{-@ measure isB @-}
-{-@ isB :: RBTree k v -> Bool @-}
-isB :: RBTree k v -> Bool
-isB Nil              = True
-isB (Node B _ _ _ _) = True
-isB (Node R _ _ _ _) = False
-
---	color invariant	--
-
-{-@ measure isRB @-}
-{-@ isRB :: RBTree k v -> Bool @-}
-isRB :: RBTree k v -> Bool
-isRB Nil              = True
-isRB (Node B _ _ l r) = isRB l
-                     && isRB r
-isRB (Node R _ _ l r) = isRB l
-                     && isRB r
-                     && isB l   
-                     && isB r
-
-{-@ measure almostRB @-}
-{-@ almostRB :: RBTree k v -> Bool @-}
-almostRB :: RBTree k v -> Bool
-almostRB Nil              = True
-almostRB (Node _ _ _ l r) = isRB l
-                         && isRB r
-
-{-@ measure blackh @-}
-{-@ blackh :: RBTree k v -> Nat @-}
-blackh :: RBTree k v -> Int
-blackh Nil              = 0
-blackh (Node R _ _ l _) = blackh l
-blackh (Node B _ _ l _) = blackh l + 1
+{-@ invariant {t:Tree k v | 0 <= height t} @-}
 
 
+
+-------------------------------------------------------------------------------
+-- | Datatypes:
+-------------------------------------------------------------------------------
+
+{-@ data Tree k v <l :: root:k -> x:k -> Bool, r :: root:k -> x:k -> Bool>
+        = Nil
+        | Node { tcol :: Color
+               , key :: k
+               , val :: v
+               , tl  :: Tree <l, r> (k <l key>) v
+               , tr  :: Tree <l, r> (k <r key>) v }
+@-}
+
+--      Ordered Trees       --
+
+{-@ type ORBT k v = Tree <{\root k -> k < root}, {\root k -> k > root}> k v @-}                           
+
+-------------------------------------------------------------------------------
+-- | Functions:
+-------------------------------------------------------------------------------
+
+{-@ makeBlack :: ORBT k v -> ORBT k v @-}
+makeBlack :: Tree k v -> Tree k v
+makeBlack Nil = Nil
+makeBlack (Node _ k v l r) = Node B k v l r
+
+{-@ set :: k -> v -> ORBT k v -> ORBT k v @-}
+set k v t = makeBlack (insert k v t)
+
+
+{-@ insert :: Ord k => k -> v -> ORBT k v -> ORBT k v @-}
+insert k v Nil = Node B k v Nil Nil
+insert k v t@(Node c key val l r)
+    | k < key   = balanceL c key val (insert k v l) r
+    | k > key   = balanceR c key val l (insert k v r)
+    | otherwise = t
+
+{-@ balanceL :: Color -> k:k -> v -> ORBT {key:k | key < k} v -> ORBT {key:k | key > k} v -> ORBT k v @-}
+balanceL B z zv (Node R y yv (Node R x xv a b) c) d = Node R y yv (Node B x xv a b) (Node B z zv c d)
+balanceL c k v l r = Node c k v l r
+
+{-@ balanceR :: Color -> k:k -> v -> ORBT {key:k | key < k} v -> ORBT {key:k | key > k} v -> ORBT k v @-}
+balanceR B y yv (Node R x xv a b) (Node R z zv c d) = Node R y yv (Node B x xv a b) (Node B z zv c d)
+balanceR col y yv x (Node R z zv c d) = Node col z zv (Node R y yv x c) d
+balanceR col x xv a b = Node col x xv a b
