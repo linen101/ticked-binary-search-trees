2fa7c7554f1748252552eead283c558023e40fa4
linen <linen@protonmail.com>
2021-01-20 18:58:36 +0200

    lifted on Tick Monad

--- src/RBTdeletion.hs
+++ src/RBTdeletion.hs
@@ -25,6 +25,13 @@ import Language.Haskell.Liquid.RTick.Combinators
 @-}
 makeRed (Node B k v l r) = Node R k v l r 
 
+{-@ makeBlackD :: t  : ARBT k v
+               -> {t' : RBT k v | size t > 0 => IsBlackRBT t' }
+@-}
+makeBlackD Nil              = Nil
+makeBlackD (Node _ k v l r) = Node B k v l r 
+
+
 -- 	Rebalancing function 
 --  after a deletion from a left subtree 
 --  of a black rooted tree
@@ -32,7 +39,7 @@ makeRed (Node B k v l r) = Node R k v l r
 
 {-@ balL :: Ord k => k:k -> v 
          -> l : ARBT {key:k | key < k} v  
-         -> r : RBTN {key:k | key > k} v {bh l+1}
+         -> r : RBTN {key:k | key > k} v {bh l+1} 
          -> {t : ARBTN k v {bh l + 1} | IsB r => isRB t}
 @-}
 balL :: Ord k => k -> v -> RBTree k v -> RBTree k v -> RBTree k v
@@ -56,53 +63,66 @@ balR y yv (Node B x xv a b) bl                 = balanceL y yv (Node R x xv a b)
 balR z zv (Node R x xv a (Node B y yv b c)) bl = Node R y yv (balanceL x xv (makeRed a) b) (Node B z zv c bl)
 
 
+-- merge 2 trees 
+-- with same bh
+
 {-@ merge :: Ord k => k:k 
           -> l : RBT {key:k | key < k} v 
-          -> r : RBTN {key:k | key > k} v {bh l} 
-          -> {t : ARBTN k v {bh l} | IsB l && IsB r => isRB t}
+          -> r : RBTN {key:k | key > k} v {bh l}
+          -> {ti : Tick {t : (ARBTN k v {bh l}) | IsB l && IsB r => isRB t} 
+                 | tcost ti <=  max (height l) (height r)}
 @-}
-merge :: Ord k => k -> RBTree k v -> RBTree k v -> RBTree k v
-merge _ Nil x                               =  x
-merge _ x Nil                               =  x
+merge :: Ord k => k -> RBTree k v -> RBTree k v -> Tick (RBTree k v)
+merge _ Nil x                               =  pure x
+merge _ x Nil                               =  pure x
 merge k (Node R x xv a b) (Node R y yv c d) = 
-    case merge k b c of
-        Node R z zv b' c'                   -> Node R z zv (Node R x xv a b') (Node R y yv c' d)
-        bc                                  -> Node R x xv a (Node R y yv bc d)
+    case tval (merge k b c) of
+        Node R z zv b' c'                   -> wait (Node R z zv (Node R x xv a b') (Node R y yv c' d))
+        bc                                  -> wait (Node R x xv a (Node R y yv bc d))
 merge k (Node B x xv a b) (Node B y yv c d) =
-    case merge k b c of 
-        Node R z zv b' c'                   -> Node R z zv (Node B x xv a b') (Node B y yv c' d)
-        bc                                  -> balL x xv a (Node B y yv c d)
-merge k a (Node R x xv b c)                 =  Node R x xv (merge k a b) c     -- IsB l && IsB r => isRB t
-merge k (Node R x xv a b) c                 =  Node R x xv a (merge k b c)     -- IsB l && IsB r => isRB t   		
+    case tval (merge k b c) of 
+        Node R z zv b' c'                   -> wait (Node R z zv (Node B x xv a b') (Node B y yv c' d))
+        bc                                  -> wait (balL x xv a (Node B y yv c d))
+merge k a (Node R x xv b c)                 =  pure (\l' -> Node R x xv l' c) <*> (merge k a b)    -- IsB l && IsB r => isRB t
+merge k (Node R x xv a b) c                 =  pure (\r' -> Node R x xv a r') <*> (merge k b c)     -- IsB l && IsB r => isRB t   		
 
+
+{-@ delete :: Ord k => k 
+                    -> t : BlackRBT k v 
+                    -> {ti : Tick (RBT k v) 
+                           | tcost ti <= height t}  
+@-}
+delete k t = fmap makeBlackD (del k t)
 
 -- termination -> decreasing parameter height t
 
 {-@ del :: Ord k => k 
            -> t : RBT k v 
-           -> {t' : ARBT k v | (if (IsB t && size t>0) then bh t' = bh t - 1 else bh t' = bh t )
-                            && (IsB t || isRB t') } 
+           -> { ti : Tick {t' : (ARBT k v) | (if (IsB t && size t>0) then bh t' = bh t - 1 else bh t' = bh t ) 
+                            && (not (IsB t) => isRB t') } 
+                   | tcost ti <= height t }
            / [height t]
 @-}
-del :: Ord k => k -> RBTree k v -> RBTree k v
-del k Nil                   = Nil
+del :: Ord k => k -> RBTree k v -> Tick (RBTree k v)
+del k Nil                   = pure Nil
 del k (Node col key v l r)  = 
     case compare k key of
         LT -> case l of
-                Nil            -> Node R key v Nil r
-                Node B _ _ _ _ -> balL key v (del k l) r
-                _              -> Node R key v (del k l) r
+                Nil            -> wait (Node R key v Nil r)
+                Node B _ _ _ _ -> pure (\l' -> balL key v l' r) </> (del k l)
+                _              -> pure (\l' -> Node R key v l' r) </> (del k l)
         GT -> case r of
-                Nil            -> Node R key v l Nil
-                Node B _ _ _ _ -> balR key v l (del k r)
-                _              -> Node R key v l (del k r)
-        EQ                     -> merge k l r
-      
+                Nil            -> wait (Node R key v l Nil)
+                Node B _ _ _ _ -> pure (\r' -> balR key v l r') </> (del k r)
+                _              -> pure (\r' -> Node R key v l r') </> (del k r)
+        EQ                     -> step 1 (merge k l r)
+     
 -------------------------------------------------------------------------------
 -- Auxiliary Invariants -------------------------------------------------------
 -------------------------------------------------------------------------------
 {-@ predicate Inv1 V = (isARB V && IsB V) => isRB V @-}
 {-@ predicate Inv2 V = isRB V => isARB V            @-}
+{-@ predicate IsBlackRBT T = bh T > 0 && IsB T @-}
 
 {-@ using (Color) as {v: Color | v = R || v = B}           @-}
 {-@ using (RBTree k v) as {v: RBTree k v | Inv1 v && Inv2 v}  @-}
